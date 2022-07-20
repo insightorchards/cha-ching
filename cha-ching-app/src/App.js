8c490c893d4e5b4836cfe7caa09113e690940d74
Jake-Mitchell <39348241+Jake-Mitchell@users.noreply.github.com>
2022-07-19 22:01:00 -0700

    adds stripe dependencies, WIP adds stripe react elements into UI

--- cha-ching-app/src/App.js
+++ cha-ching-app/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentElement } from "@stripe/react-stripe-js";
+
+import "./App.css";
+
+const stripePromise = loadStripe(process.env.PUBLIC_STRIPE_KEY);
+console.log(process.env.PUBLIC_STRIPE_KEY);
 
 function App() {
   return (
     <div>
-      <div className="header">
-        This is Cha-ching
-      </div>
+      <div className="header">This is Cha-ching</div>
+      <Elements stripe={stripePromise}>
+        <PaymentElement />
+      </Elements>
     </div>
   );
 }
