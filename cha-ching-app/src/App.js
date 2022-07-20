5ad1fc9158fa9a826b83320b79deb5e8dcbff4ab
Charles Magic Woo <codepath2019@gmail.com>
2022-07-19 22:39:05 -0700

    WIP paymentElement setup

--- cha-ching-app/src/App.js
+++ cha-ching-app/src/App.js
@@ -4,14 +4,20 @@ import { PaymentElement } from "@stripe/react-stripe-js";
 
 import "./App.css";
 
-const stripePromise = loadStripe(process.env.PUBLIC_STRIPE_KEY);
-console.log(process.env.PUBLIC_STRIPE_KEY);
+const stripePromise = loadStripe(
+  "pk_test_51LN2YSDXZ2nrCEJAoQsVYTK57pNjgCcaXzd3SQKlI41uKS0C5l4IDZXPMd4auY5dJuJFChCQzEMUliwD82kpBv1x00C1E0BuBJ",
+);
+console.log(stripePromise);
 
 function App() {
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: "{{'hallo'}}",
+  };
   return (
     <div>
       <div className="header">This is Cha-ching</div>
-      <Elements stripe={stripePromise}>
+      <Elements options={options} stripe={stripePromise}>
         <PaymentElement />
       </Elements>
     </div>
