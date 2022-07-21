ef1354452ce87e4b15e38325ffec070d33df6793
Charles Magic Woo <codepath2019@gmail.com>
2022-07-21 11:28:19 -0700

    sets up jest for server tests

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -2,23 +2,24 @@ const supertest = require("supertest");
 const assert = require("assert");
 const app = require("../server");
 const request = supertest(app);
+// import { jest } from "@jest/globals";
 
-// describe("app", () => {
-//   it("sends a response from root", () => {
-//     request
-//       .get("/")
-//       .expect("Hello World!")
-//       .expect(200)
-//       .end(function (err, res) {
-//         if (err) throw err;
-//       });
-//   });
-// });
-
-it("Gets the test endpoint", async (done) => {
-  // Sends GET Request to /test endpoint
-  const res = await request.get("/test");
-
-  // ...
-  done();
+describe("app", () => {
+  it("sends a response from root", () => {
+    request
+      .get("/")
+      .expect("Hello World!")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+      });
+  });
 });
+
+// it("Gets the test endpoint", async (done) => {
+//   // Sends GET Request to /test endpoint
+//   const res = await request.get("/test");
+
+//   // ...
+//   done();
+// });
