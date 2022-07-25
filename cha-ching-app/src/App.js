dd0e0b7fc9800d99e120a4fb7af293f92b8a3812
Jake-Mitchell <39348241+Jake-Mitchell@users.noreply.github.com>
2022-07-24 18:03:38 -0700

    shows loading until page gets paymentIntent, shows payment fields on page load, adds env.sample files, patch server app so it now runs

--- cha-ching-app/src/App.js
+++ cha-ching-app/src/App.js
@@ -1,6 +1,6 @@
 import { Elements, PaymentElement } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const stripePromise = loadStripe(process.env.REACT_APP_PUBLIC_STRIPE_KEY);
@@ -10,32 +10,30 @@ function App() {
 
   const isEmpty = (object) => Object.keys(object).length === 0;
 
-  function getClientSecret() {
+  useEffect(() => {
     fetch("http://localhost:3001/payment-intent")
       .then((res) => res.json())
       .then((data) => {
         setOptions(data);
       });
-  }
+  }, []);
 
-  return (
+  return !isEmpty(options) ? (
     <div>
       <div className="header">Cha-ching</div>
       <div className="paymentFormContainer">
         <div className="paymentElementcontainer">
-          {!isEmpty(options) ? (
-            <Elements options={options} stripe={stripePromise}>
-              <PaymentElement />
-            </Elements>
-          ) : (
-            "loading"
-          )}
+          <Elements options={options} stripe={stripePromise}>
+            <PaymentElement />
+          </Elements>
         </div>
-        <button onClick={() => getClientSecret()} className="button">
+        <button onClick={() => {}} className="button">
           Submit
         </button>
       </div>
     </div>
+  ) : (
+    "loading"
   );
 }
 
