50f96f5076401189fafa96863ed5f65841f3b8a8
Jake-Mitchell <39348241+Jake-Mitchell@users.noreply.github.com>
2022-07-27 10:16:17 -0700

    correctly runs a jest supertest

--- server/app.js
+++ server/app.js
@@ -1,39 +1,43 @@
-const app = require("./server.js");
-const express = require("express");
-const port = 3001;
-const cors = require("cors");
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env") });
-const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+const app = require("./server.js")
+const express = require("express")
+// const app = express()
+const port = 3001
+const cors = require("cors")
+const path = require("path")
+require("dotenv").config({ path: path.join(__dirname, ".env") })
+const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.get("/", (req, res) => {
-  res.json({ juice: "beatle juice" });
-});
+  res.json({ juice: "beatle juice" })
+})
 
 app.get("/success", (req, res) => {
-  res.send("Success!");
-});
+  res.send("Success!")
+})
 
 app.get("/payment-intent", async (req, res) => {
   const paymentIntent = await stripe.paymentIntents.create({
     amount: 2000,
     currency: "usd",
-  });
-  res.json({ clientSecret: paymentIntent.client_secret });
-});
+  })
+  res.json({ clientSecret: paymentIntent.client_secret })
+})
 
 app.post("/", (req, res) => {
-  res.json({ drink: "banana soda" });
-  console.log("POST REQUEST BODY", req.body);
-});
+  res.json({ drink: "banana soda" })
+  console.log("POST REQUEST BODY", req.body)
+})
 
 app.get("/test", async (req, res) => {
-  res.json({ message: "pass!" });
-});
+  res.json({ message: "pass!" })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = app
--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -1,21 +1,34 @@
-const supertest = require("supertest");
-const assert = require("assert");
-const app = require("../server");
-const request = supertest(app);
+const request = require("supertest")
+const app = require("../app")
+// console.log("app", app)
+// const assert = require("assert")
+// const request = supertest(app);
 // import { jest } from "@jest/globals";
 
 describe("app", () => {
   it("sends a response from root", () => {
-    request
+    request(app)
       .get("/")
-      .expect("Hello World!")
       .expect(200)
+      .expect({ juice: "beatle juice" })
       .end(function (err, res) {
-        if (err) throw err;
-      });
-  });
-});
+        if (err) throw err
+      })
+  })
+})
 
+describe.only("test", () => {
+  it("is a test", async () => {
+    const result = await await request(app).get("/")
+    expect(result.statusCode).toBe(200)
+    expect(result.body).toEqual(
+      expect.objectContaining({
+        juice: "beatle juice",
+      })
+    )
+    // console.log(result)
+  })
+})
 // it("Gets the test endpoint", async (done) => {
 //   // Sends GET Request to /test endpoint
 //   const res = await request.get("/test");
