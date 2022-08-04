af4ab0fa7cc289564a2150decaa8de706d7bda25
Angeline Polidano <angelinepolidano@gmail.com>
2022-08-03 21:16:12 -0700

    fixes app creashing by removing second res.json, prettier changes

--- cha-ching-app/src/ElementsProviderCheckout.js
+++ cha-ching-app/src/ElementsProviderCheckout.js
@@ -1,27 +1,26 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from './CheckoutForm'
+import CheckoutForm from "./CheckoutForm";
 import Error from "./Error";
 import Loading from "./Loading";
 import { useLocation } from "react-router-dom";
 
 const stripePromise = loadStripe(process.env.REACT_APP_PUBLIC_STRIPE_KEY);
 const ElementsProviderCheckout = () => {
+  const isEmpty = (object) => Object.keys(object).length === 0;
+  const { state: options } = useLocation();
 
-    const isEmpty = (object) => Object.keys(object).length === 0;
-    const { state: options } = useLocation()
-
-    return !isEmpty(options) ? (
-        (options.clientSecret === null) ? (
-            <Error />
-        ) : (
-            <Elements options={options} stripe={stripePromise}>
-                <CheckoutForm />
-            </Elements>
-        )
+  return !isEmpty(options) ? (
+    options.clientSecret === null ? (
+      <Error />
     ) : (
-        <Loading />
-    );
+      <Elements options={options} stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
+    )
+  ) : (
+    <Loading />
+  );
 };
 
 export default ElementsProviderCheckout;
--- server/app.js
+++ server/app.js
@@ -1,103 +1,103 @@
-const app = require("./server.js")
-const express = require("express")
-const port = 3001
-const cors = require("cors")
-const path = require("path")
-require("dotenv").config({ path: path.join(__dirname, ".env") })
-const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY)
+const app = require("./server.js");
+const express = require("express");
+const port = 3001;
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
+const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({ test: "hello world!" })
-})
+  res.json({ test: "hello world!" });
+});
 
 app.post("/create-incomplete-subscription", async (req, res) => {
-  console.log("req.body", req.body)
+  console.log("req.body", req.body);
   const productResult = await stripe.products.create({
     name: req.body.name,
-  })
+  });
   const priceResult = await stripe.prices.create({
-    currency: 'usd',
+    currency: "usd",
     product: productResult.id,
     unit_amount: req.body.amount,
     recurring: {
-      interval: "month"
-    }
-  })
+      interval: "month",
+    },
+  });
 
   const customer = await stripe.customers.create({});
 
   const subscription = await stripe.subscriptions.create({
     customer: customer.id,
-    items: [
-      {price: priceResult.id},
-    ],
-    payment_behavior: 'default_incomplete',
-    payment_settings: { save_default_payment_method: 'on_subscription' },
-    expand: ['latest_invoice.payment_intent'],
+    items: [{ price: priceResult.id }],
+    payment_behavior: "default_incomplete",
+    payment_settings: { save_default_payment_method: "on_subscription" },
+    expand: ["latest_invoice.payment_intent"],
   });
 
-  res.json(subscription)
-  console.log("subscription", subscription.latest_invoice.payment_intent.client_secret)
-
-  res.json({ result: priceResult })
-})
+  res.json(subscription);
+  console.log(
+    "subscription",
+    subscription.latest_invoice.payment_intent.client_secret,
+  );
+});
 
 app.get("/subscriptions", async (req, res) => {
   const product = await stripe.products.create({
-    name: 'Jelly Bean',
-  })
+    name: "Jelly Bean",
+  });
 
   const price = await stripe.prices.create({
-    currency: 'usd',
+    currency: "usd",
     product: product.id,
     unit_amount: 2000,
     recurring: {
-      interval: "month"
-    }
-  })
+      interval: "month",
+    },
+  });
 
   const paymentMethod = await stripe.paymentMethods.create({
-    type: 'card',
+    type: "card",
     card: {
-      number: '4242424242424242',
+      number: "4242424242424242",
       exp_month: 7,
       exp_year: 2023,
-      cvc: '314',
+      cvc: "314",
     },
-  })
+  });
 
   const customer = await stripe.customers.create({
     email: "jojobob@example.com",
     name: "Jojo Bob",
-    payment_method: paymentMethod.id
+    payment_method: paymentMethod.id,
   });
 
   const subscription = await stripe.subscriptions.create({
     customer: customer.id,
-    items: [
-      {price: price.id},
-    ],
+    items: [{ price: price.id }],
     default_payment_method: paymentMethod.id,
-    payment_behavior: 'default_incomplete',
-    payment_settings: { save_default_payment_method: 'on_subscription' },
-    expand: ['latest_invoice.payment_intent'],
+    payment_behavior: "default_incomplete",
+    payment_settings: { save_default_payment_method: "on_subscription" },
+    expand: ["latest_invoice.payment_intent"],
   });
 
-  res.json(subscription)
-  console.log("subscription", subscription.latest_invoice.payment_intent.client_secret)
-})
+  res.json(subscription);
+  console.log(
+    "subscription",
+    subscription.latest_invoice.payment_intent.client_secret,
+  );
+});
 
 app.get("/payment-intent", async (req, res) => {
   const paymentIntentResult = await stripe.paymentIntents.create({
     amount: 2000,
     currency: "usd",
-  })
-  res.json({ clientSecret: paymentIntentResult.client_secret })
-})
+  });
+  res.json({ clientSecret: paymentIntentResult.client_secret });
+});
 
 app.post("/payment-intent", async (req, res, next) => {
   const paymentIntentResult = await stripe.paymentIntents
@@ -106,19 +106,21 @@ app.post("/payment-intent", async (req, res, next) => {
       currency: "usd",
     })
     .catch((err) => {
-      const errorStatusCode = err.statusCode
-      const errorMessage = err.raw.message
-      return { error: errorMessage, statusCode: errorStatusCode }
-    })
+      const errorStatusCode = err.statusCode;
+      const errorMessage = err.raw.message;
+      return { error: errorMessage, statusCode: errorStatusCode };
+    });
 
-  res.statusCode = paymentIntentResult.error ? 400 : 200
-  res.json(paymentIntentResult)
-})
+  res.statusCode = paymentIntentResult.error ? 400 : 200;
+  res.json(paymentIntentResult);
+});
 
 // how do I get enforce signature to take in enforceSignature(_signatureObject, req, res, next, errorStacker)
 const enforceSignature = (req, res, next) => {
-  console.log("WIP, continue investigation into error handling with middlewear")
-  next()
+  console.log(
+    "WIP, continue investigation into error handling with middlewear",
+  );
+  next();
   // // if(req.headers.type !== json) error handling call an errorStack() function or something
   // if (typeof req.body.amount !== "object") {
   //   throw new Error(
@@ -133,7 +135,7 @@ const enforceSignature = (req, res, next) => {
   //   // throw or just run a response object ?
   //   next()
   // }
-}
+};
 
 app.get("/stripe-client-secret", enforceSignature, async (req, res) => {
   const paymentIntentResult = await stripe.paymentIntents
@@ -142,20 +144,20 @@ app.get("/stripe-client-secret", enforceSignature, async (req, res) => {
       currency: "usd",
     })
     .then((data) => {
-      return { ...data, clientSecret: data.client_secret }
+      return { ...data, clientSecret: data.client_secret };
     })
     .catch((err) => {
-      const errorStatusCode = err.statusCode
-      const errorMessage = err.raw.message
-      return { error: errorMessage, errorStatusCode }
-    })
+      const errorStatusCode = err.statusCode;
+      const errorMessage = err.raw.message;
+      return { error: errorMessage, errorStatusCode };
+    });
 
-  res.statusCode = paymentIntentResult.error ? 400 : 200
-  res.json(paymentIntentResult)
-})
+  res.statusCode = paymentIntentResult.error ? 400 : 200;
+  res.json(paymentIntentResult);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-module.exports = app
+module.exports = app;
