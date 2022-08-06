2c7b8db7bfcdf2e8c79a16d6c58bdd5df2d6e8db
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-08-06 13:47:20 -0700

    Add initial scafolding for scrollable landing page and set root route to render new ScrollableLandingPage component

---
+++ cha-ching-app/src/ScrollableLandingPage.js
@@ -0,0 +1,12 @@
+import SubscriptionPage from './SubscriptionPage';
+
+function ScrollableLandingPage() {
+    return(
+        <div>
+            <div>Placeholder for Hero page</div>
+            <SubscriptionPage/>
+        </div>
+    )
+}
+
+export default ScrollableLandingPage;
