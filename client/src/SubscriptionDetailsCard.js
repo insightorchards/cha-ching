da5f3c64493d279dab8e0c5b159346a64a8aaa3e
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-08-14 22:11:51 -0700

    add in props that allow component to hightlight when selected

--- client/src/SubscriptionDetailsCard.js
+++ client/src/SubscriptionDetailsCard.js
@@ -1,8 +1,32 @@
 import "./SubscriptionDetailsCard.css";
+import { useState, useEffect } from "react";
 
-const SubscriptionDetailsCard = () => {
+const SubscriptionDetailsCard = ({ id, selectedCardId }) => {
+    const [hovered, setHovered] = useState(false);
+    const [selected, setSelected] = useState(false);
+
+    const determineCardWasSelected = () => {
+        if(id != undefined && selectedCardId != undefined){
+            if(id === selectedCardId){ setSelected(true) }
+        }
+    };
+
+    useEffect(() => {
+        determineCardWasSelected()
+        if(id !== selectedCardId){ setSelected(false) }
+    }, [ selected ])
+
+    const determineStylingBasedOnUserAction = () => {
+        if(hovered && !selected) { return "hoveredStyling" }
+        if(selected) { return "selectedStyling" }
+    };
+
     return(
-        <div className="container"></div>
+        <div
+            className={`${determineStylingBasedOnUserAction()} container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        />
     )
 };
 
