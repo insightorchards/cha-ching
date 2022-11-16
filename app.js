e824df35a6bc02c6e9e575b4bbc0b250657b91fc
Ashlynn223 <50315144+Ashlynn223@users.noreply.github.com>
2022-11-15 23:29:40 -0800

    add global repose headers in express app

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ test: "hello cha-chingers!" });
 });
